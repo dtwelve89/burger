139818ce9d94b338fb825b8bf48a3511e24bb935
Danny Ocean <danny.danh.nguyen@gmail.com>
2018-08-22 12:48:19 -0700

    almost

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double-Double Animal Chiles');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
